42410471389b2a19e74d5995ad185271cceebc59
Keith Armstrong <keith1015@gmail.com>
2014-12-16 19:17:32 -0600

    Optimized code

--- js/navigation.js
+++ js/navigation.js
@@ -3,156 +3,67 @@ var map = document.getElementById('map');
 var bump = new buzz.sound('sounds/bump.m4a');
 var boundaries;
 var warps;
-var x, y, xamt, yamt;
 var pos = [+$("#trainer").css("left").replace(/[^-\d\.]/g, ''),+$("#trainer").css("top").replace(/[^-\d\.]/g, '')+6];
 var tilepos = [(+$("#trainer").css("left").replace(/[^-\d\.]/g, '')+16)/16,(+$("#trainer").css("top").replace(/[^-\d\.]/g, '')+22)/16];
 var godmode = false;
 var showpos = false;
-var delay = 150;
+var delay = 125;
 var walking = false;
+var facing;
+var UP = 0
+var RIGHT = 1
+var DOWN = 2
+var LEFT = 3
+var directions = ["up", "right", "down", "left"];
+var arrows = [38, 39, 40, 37];
+var amt = {
+    0: {left: 0, top: -16, xtile: 0, ytile: -1},
+    1: {left: 16, top: 0, xtile: 1, ytile: 0},
+    2: {left: 0, top: 16, xtile: 0, ytile: 1},
+    3: {left: -16, top: 0, xtile: -1, ytile: 0}
+};
 
-// Load boundaries and warps
+var dirs = {
+    38: 0,
+    39: 1,
+    40: 2,
+    37: 3
+};
+
+// Load boundaries and warps initially
 loadBoundaries();
 loadWarps();
 
-// Start music
+// Start playing music
 playMusic($("music").html());
 
-// Fetch boundary data
-function loadBoundaries() {
-    var id = $('id').html();
-    $.getJSON("boundaries/" + id + ".json", function( data ) {
-        boundaries = data;
-    });
-}
-
-function loadWarps() {
-    var id = $('id').html();
-    $.getJSON("warps/" + id + ".json", function( data ) {
-        warps = data;
-    });
-}
-
+// Key events
 $(document).keydown(function(e) {
-    if (e.which == 39 || e.which == 37 || e.which == 38 || e.which == 40) {
+    // Only move if proper key is used
+    if (inArray(e.which, arrows)) {
         e.preventDefault();
-    }
-    steps++;
-    // Right
-    if (e.which == 39) {
-        if (!checkWarps(1)) {
-            if (+pos[0]+16 < map.naturalWidth && validMove(1) && !walking || godmode) {
-                $('#trainer').attr("src", "img/sprites/trainer_right_" + ((steps%3)+1) + ".png");
-                walking = true;
-                $( "#trainer" ).animate({
-                    left: "+=16",
-                }, delay, function() {
-                    walking = false;
-                    updatePos();
-                });
-                //$("#trainer").css("left", "+=16");
-            } else {
-                if (!walking) {
-                    bump.play();
-                }
-            }
-        }
-    // Left
-    } else if (e.which == 37) {
-        if (!checkWarps(3)) {
-            if (+pos[0]-16 >= 0 && validMove(3) && !walking || godmode) {
-                $('#trainer').attr("src", "img/sprites/trainer_left_" + ((steps%3)+1) + ".png");
-                walking = true;
-                $( "#trainer" ).animate({
-                    left: "-=16",
-                }, delay, function() {
-                    walking = false;
-                    updatePos();
-                });
-                //$("#trainer").css("left", "-=16");
-            } else {
-                if (!walking) {
-                    bump.play();
-                }
-            }
-        }
-    // Down
-    } else if (e.which == 40) {
-        if (!checkWarps(2)) {
-            if (+pos[1]+26 < map.naturalHeight && validMove(2) && !walking || godmode) {
-                $('#trainer').attr("src", "img/sprites/trainer_front_" + ((steps%3)+1) + ".png");
-                walking = true;
-                $( "#trainer" ).animate({
-                    top: "+=16",
-                }, delay, function() {
-                    walking = false;
-                    updatePos();
-                });
-                //$("#trainer").css("top", "+=16");
-            } else {
-                if (!walking) {
-                    bump.play();
-                }
-            }
-        }
-    // Up
-    } else if (e.which == 38) {
-        if (!checkWarps(0)) {
-            if (+pos[1]-6 > 0 && validMove(0) && !walking || godmode) {
-                $('#trainer').attr("src", "img/sprites/trainer_back_" + ((steps%3)+1) + ".png");
-                walking = true;
-                $( "#trainer" ).animate({
-                    top: "-=16",
-                }, delay, function() {
-                    walking = false;
-                    updatePos();
-                });
-                //$("#trainer").css("top", "-=16");
-            } else {
-                if (!walking) {
-                    bump.play();
-                }
-            }
-        }
+        // Increase step counter
+        steps++;
+
+        // Move in direciton
+        move(dirs[e.which]);
     }
 });
 
 function validMove(direction) {
+    var invalidMove = false;
     if (godmode) {
         return true;
     }
-    var invalidMove = false;
-    // Get the correct x and y coords of player
-    x = +pos[0];
-    y = +pos[1];
-
-    switch (direction) {
-        case 0:
-            xamt = 0;
-            yamt = -16;
-            break;
-        case 1:
-            xamt = 16;
-            yamt = 0;
-            break;
-        case 2:
-            xamt = 0;
-            yamt = 16;
-            break;
-        case 3:
-            xamt = -16;
-            yamt = 0;
-            break;
-    }
-
 
     // Check if player is going to hit any of the boundaries
     $.each(boundaries, function(index, value) {
-        if (Math.abs(pos[0]+xamt-value["x"]) < 16 && Math.abs(pos[1]+yamt-value["y"]) < 16) {
+        if (Math.abs(pos[0]+amt[direction].left-value["x"]) < 16 && Math.abs(pos[1]+amt[direction].top-value["y"]) < 16) {
             invalidMove = true;
             return false;
         }
     });
+
     if (invalidMove) {
         return false;
     } else {
@@ -164,44 +75,17 @@ function updatePos() {
     pos = [+$('#trainer').css("left").replace(/[^-\d\.]/g, ''), +$('#trainer').css("top").replace(/[^-\d\.]/g, '')+6];
     tilepos = [(+$("#trainer").css("left").replace(/[^-\d\.]/g, '')+16)/16,(+$("#trainer").css("top").replace(/[^-\d\.]/g, '')+22)/16];
     if (showpos) {
-        console.log(pos + " [" + tilepos + "]");
+        console.log("X: " + pos[0] + " [" + tilepos[0] + "]\n Y: " + pos[1] + " [" + tilepos[1] + "]\n Direction: " + facing + " [" + directions[facing] + "]");
     }
 }
 
 // If player moves into warp spot, load in new data else normal move action
-function checkWarps(direction) {
-    switch (direction) {
-        case 0:
-            xamt = 0;
-            yamt = -16;
-            xtile = 0;
-            ytile = -1;
-            break;
-        case 1:
-            xamt = 16;
-            yamt = 0;
-            xtile = 1;
-            ytile = 0;
-            break;
-        case 2:
-            xamt = 0;
-            yamt = 16;
-            xtile = 0;
-            ytile = 1;
-            break;
-        case 3:
-            xamt = -16;
-            yamt = 0;
-            xtile = -1;
-            ytile = 0;
-            break;
-    }
+function isWarp(direction) {
     var src_x;
     var src_y;
     var dst_x;
     var dst_y;
     var dst_direction;
-    var directions = ["back", "right", "front", "left"];
     var valid = false;
     $.each(warps, function(index, warp) {
         src_x = +warp.src_coords["x"];
@@ -212,8 +96,8 @@ function checkWarps(direction) {
 
         dst_direction = warp.dst_direction;
 
-        // Move to tile but not require move after in certain direction
-        if ((tilepos[0]+xtile == src_x && tilepos[1]+ytile == src_y && warp.src_direction === false) || tilepos[0] == src_x && tilepos[1] == src_y && +warp.src_direction === direction && warp.src_direction !== false) {
+        // Valid warp
+        if ((tilepos[0]+amt[direction].xtile == src_x && tilepos[1]+amt[direction].ytile == src_y && warp.src_direction === false) || tilepos[0] == src_x && tilepos[1] == src_y && +warp.src_direction === direction && warp.src_direction !== false) {
             // Update map id
             $('id').html(warp.map);
 
@@ -266,10 +150,81 @@ function checkWarps(direction) {
                 });
             });
 
+            // Update player's direction
+            facing = direction;
+
             // Don't move player in direction they were headed since this was a valid warp
             valid = true;
         }
     });
 
     return valid;
+}
+
+// Fetch boundary data
+function loadBoundaries() {
+    var id = $('id').html();
+    $.getJSON("boundaries/" + id + ".json", function( data ) {
+        boundaries = data;
+    });
+}
+
+function loadWarps() {
+    var id = $('id').html();
+    $.getJSON("warps/" + id + ".json", function( data ) {
+        warps = data;
+    });
+}
+
+function inArray(value, array) {
+  return array.indexOf(value) > -1;
+}
+
+function move(direction) {
+    // Check if player is moving towards warp
+    if (!isWarp(direction)) {
+        // If play isn't currently walking, update sprite animation
+        if (!walking) {
+            $('#trainer').attr("src", "img/sprites/trainer_" + directions[direction] + "_" + ((steps%3)+1) + ".png");
+        }
+        // Update direction character is facing
+        facing = direction;
+
+        // Check if player is allowed to move in direction
+        if (withinBounds(direction) && validMove(direction) && !walking || godmode) {
+            walking = true;
+
+            // Animate player walking with delay like ingame
+            $( "#trainer" ).animate({
+                left: "+=" + amt[direction].left,
+                top: "+=" + amt[direction].top,
+            }, delay, function() {
+                walking = false;
+                updatePos();
+            });
+        } else {
+            if (!walking) {
+                walking = true;
+                $( "#trainer" ).animate({
+                    left: "=",
+                }, delay, function() {
+                    walking = false;
+                });
+                bump.play();
+            }
+        }
+    }
+}
+
+function withinBounds(direction) {
+    if (direction == UP) {
+        return +pos[1]-6 > 0;
+    } else if (direction == RIGHT) {
+        return +pos[0]+16 < map.naturalWidth;
+    } else if (direction == DOWN) {
+        return +pos[1]+26 < map.naturalHeight;
+    } else if (direction == LEFT) {
+        return +pos[0]-16 >= 0;
+    }
+    return false;
 }
